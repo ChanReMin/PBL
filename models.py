771a7ee5e66a95ff7bb4b803f5ee4a6b465ed469
ChanReMin <minhletran152003@gmail.com>
2023-11-06 19:41:55 +0700

    update

---
+++ models.py
@@ -0,0 +1,13 @@
+
+class Fruit:
+    def __init__(self, id, name, description, sold, exist, image, price):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.sold = sold
+        self.exist = exist
+        self.image = image
+        self.price = price
+
+
+
