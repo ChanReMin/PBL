16e9d031d9f64ccb9e62766a7ae900e4a49c1689
Giabao5443 <giabao5443@gmail.com>
2023-12-23 21:08:17 +0700

    💁

---
+++ user_model.py
@@ -0,0 +1,10 @@
+class user:
+    def __init__(self,email,password,name,phone,address,birth,sex):
+        self.id = id
+        self.email = email
+        self.password = password
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.birth = birth
+        self.sex = sex
